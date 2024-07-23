55573f635a5fb3667c512548f45ea3c25f3e0555
DanielAlmeida19 <dani1114vivilinda@gmail.com>
2024-07-23 10:24:03 -0300

    Criando a URL, o template e a view para criação de contatos

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from contact.models import Contact
+
+
+def create(request):
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .order_by('-id')
+
+    context = {}
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
